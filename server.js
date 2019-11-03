ef8c7cbdb809e7dbe236a622c879e89e98e26fd1
artem kosarev <aionics@list.ru>
2019-11-03 03:48:21 +0300

    nodemon added, config added

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+const config = require('./config')
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.sendFile('index.html')
 })
 
-app.listen(5000)
+app.listen(config.PORT, (err) => {
+    if (err) {
+        console.error(err)
+    }
+    console.log(`listening on ${config.PORT}`)
+})
