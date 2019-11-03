0e7eb8b98b6ffa2cf352f8c933253c526e85303c
artem kosarev <aionics@list.ru>
2019-11-03 17:03:41 +0300

    state update

--- socket_handler.js
+++ socket_handler.js
@@ -1,24 +1,41 @@
+const cookie = require('cookie')
+
 module.exports = (http) => {
     const socket_server = require('socket.io')(http);
     let socket_connections = {}
     socket_server.on('connection', (socket_conenction) => {
-        console.log('got connection')
-        socket_conenction.qurey = []
+        socket_conenction.query = []
         socket_conenction.on('disconnect', () => {
-            console.log(`${socket_conenction.uuid} has disconnected`)
-            delete socket_connections[socket_conenction.uuid]
+            console.log(`${socket_conenction.user_id} has disconnected`)
+
+            socket_conenction.query.push({
+                action: "connection",
+                enum: 'disconnected',
+                user_id: socket_conenction.user_id
+
+            })
         })
 
 
-        socket_conenction.on('/hello', (uuid) => {
-            socket_conenction.uuid = uuid
-            socket_connections[uuid] = socket_conenction
-            console.log(`${uuid} has been initialized`)
+        socket_conenction.on('/hello', (user_id) => {
+            cookies = cookie.parse(socket_conenction.request.headers.cookie)
+
+            socket_conenction.user_id = cookies.user_id
+            socket_connections[cookies.user_id] = socket_conenction
+            console.log(`${cookies.user_id} has been initialized`)
+
+            socket_conenction.query.push({
+                action: "connection",
+                enum: 'connected',
+                user_id: cookies.user_id
+            })
+
         })
 
         socket_conenction.on('/event', (action) => {
-            socket_conenction.qurey.push(action)
+            socket_conenction.query.push(action)
         })
+
     })
     return socket_connections
 }
